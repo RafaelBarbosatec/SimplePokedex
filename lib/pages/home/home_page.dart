3b37dfcd2adb51b1bdfe1d004678e7750d62e9c4
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-07-02 14:36:21 -0300

    atualiza layout

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,9 +5,7 @@ import 'package:simple_pokedex/pages/home/bloc/home_events.dart';
 import 'package:simple_pokedex/pages/home/widgets/header.dart';
 import 'package:simple_pokedex/pages/home/widgets/pokemon_empty.dart';
 import 'package:simple_pokedex/pages/home/widgets/pokemon_item.dart';
-import 'package:simple_pokedex/pages/home/widgets/pokemon_type_list.dart';
 import 'package:simple_pokedex/repository/pokemon/model/pokemon.dart';
-import 'package:simple_pokedex/repository/pokemon/model/pokemon_type.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -21,7 +19,8 @@ class HomePage extends StatelessWidget {
             return Stack(
               children: <Widget>[
                 _buildContent(context, communication),
-                _buildProgress(communication)
+                _buildProgress(communication),
+                _buildEmpty(communication),
               ],
             );
           },
@@ -35,56 +34,31 @@ class HomePage extends StatelessWidget {
       children: <Widget>[
         Header(communication: communication),
         SizedBox(
-          height: 10,
+          height: 2,
         ),
-        _buildTypes(communication),
-        Expanded(
-          child: _buildPokemonList(communication),
-        )
+        _buildPokemonList(communication)
       ],
     );
   }
 
-  Widget _buildTypes(HomeCommunication communication) {
-    return communication.pokemonsTypes.builder<List<PokemonType>>((types) {
-      return PokemonTypeList(
-        types: types,
-        typeSelected: (type) {
-          communication.dispatcher(SelectType(type));
-        },
-      );
-    });
-  }
-
   Widget _buildPokemonList(HomeCommunication communication) {
-    return communication.pokemons.builder<List<Pokemon>>((list) {
-      return Stack(
-        children: <Widget>[
-          ListView.builder(
-              padding: EdgeInsets.only(top: 10, bottom: 16),
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                if (index == list.length - 3) {
-                  communication.dispatcher(LoadPokemons(true));
-                }
+    return Expanded(
+      child: communication.pokemons.builder<List<Pokemon>>((list) {
+        return ListView.builder(
+          padding: EdgeInsets.only(bottom: 16),
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            if (index == list.length - 3) {
+              communication.dispatcher(LoadPokemons(true));
+            }
 
-                return PokemonItem(
-                  pokemon: list[index],
-                );
-              }),
-          Container(
-            height: 10,
-            decoration: BoxDecoration(color: Colors.white, boxShadow: [
-              BoxShadow(
-                  blurRadius: 4,
-                  offset: Offset(0.0, 5),
-                  color: Colors.grey[300])
-            ]),
-          ),
-          _buildEmpty(communication),
-        ],
-      );
-    });
+            return PokemonItem(
+              pokemon: list[index],
+            );
+          },
+        );
+      }),
+    );
   }
 
   Widget _buildProgress(HomeCommunication streams) {
--- lib/pages/home/widgets/header.dart
+++ lib/pages/home/widgets/header.dart
@@ -1,6 +1,9 @@
+import 'package:bsev/bsev.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_pokedex/pages/home/bloc/home_bloc.dart';
 import 'package:simple_pokedex/pages/home/bloc/home_events.dart';
+import 'package:simple_pokedex/pages/home/widgets/pokemon_type_list.dart';
+import 'package:simple_pokedex/repository/pokemon/model/pokemon_type.dart';
 
 class Header extends StatelessWidget {
   final HomeCommunication communication;
@@ -8,55 +11,71 @@ class Header extends StatelessWidget {
   const Header({Key key, this.communication}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, top: 20),
-              child: Text(
-                'SimplePokedex',
-                style: Theme.of(context)
-                    .textTheme
-                    .title
-                    .copyWith(color: Colors.grey[700]),
+    return Card(
+      margin: EdgeInsets.only(),
+      elevation: 4,
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0, top: 20),
+                child: Text(
+                  'SimplePokedex',
+                  style: Theme.of(context)
+                      .textTheme
+                      .title
+                      .copyWith(color: Colors.grey[700]),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0, top: 20),
-              child: Image.asset(
-                'assets/pokebola_img.png',
-                height: 30,
-                width: 30,
-              ),
-            )
-          ],
-        ),
-        SizedBox(
-          height: 25,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(30)),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: TextField(
-                onChanged: (name) {
-                  communication.dispatcher(SearchName(name));
-                },
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    hintText: 'Search per name'),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0, top: 20),
+                child: Image.asset(
+                  'assets/pokebola_img.png',
+                  height: 30,
+                  width: 30,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(30)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  onChanged: (name) {
+                    communication.dispatcher(SearchName(name));
+                  },
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      hintText: 'Search per name'),
+                ),
               ),
             ),
           ),
-        ),
-      ],
+          _buildTypes(communication),
+        ],
+      ),
     );
+  }
+
+  Widget _buildTypes(HomeCommunication communication) {
+    return communication.pokemonsTypes.builder<List<PokemonType>>((types) {
+      return PokemonTypeList(
+        types: types,
+        typeSelected: (type) {
+          communication.dispatcher(SelectType(type));
+        },
+      );
+    });
   }
 }
