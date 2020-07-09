80ce439150fe85dd26f28f3045e829f50ecbbc41
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-07-09 12:28:06 -0300

    remove con

--- lib/di/inject_dependencies.dart
+++ lib/di/inject_dependencies.dart
@@ -1,7 +1,7 @@
 import 'package:bsev/bsev.dart';
+import 'package:dio/dio.dart';
 import 'package:simple_pokedex/pages/home/bloc/home_bloc.dart';
 import 'package:simple_pokedex/repository/pokemon/pokemon_repository.dart';
-import 'package:simple_pokedex/util/con.dart';
 
 void injectDependencies() {
   injectUtils();
@@ -16,6 +16,8 @@ void injectBloCs() {
 }
 
 void injectUtils() {
-  registerSingletonDependency((i) => Con('http://104.131.18.84/', debug: true));
+  registerSingletonDependency(
+    (i) => Dio(BaseOptions(baseUrl: 'http://104.131.18.84/')),
+  );
   registerSingletonDependency((i) => PokemonRepository(i.get()));
 }
--- lib/repository/pokemon/pokemon_repository.dart
+++ lib/repository/pokemon/pokemon_repository.dart
@@ -1,28 +1,32 @@
+import 'package:dio/dio.dart';
 import 'package:simple_pokedex/repository/pokemon/model/pokemon.dart';
 import 'package:simple_pokedex/repository/pokemon/model/pokemon_type.dart';
-import 'package:simple_pokedex/util/con.dart';
 
 class PokemonRepository {
-  final Con _con;
+  final Dio _api;
 
-  PokemonRepository(this._con);
+  PokemonRepository(this._api);
 
-  Future<List<Pokemon>> getPokemonList(
-      {int page = 0, int limit, String name, String type}) {
+  Future<List<Pokemon>> getPokemonList({
+    int page = 0,
+    int limit,
+    String name,
+    String type,
+  }) {
     Map<String, dynamic> params = Map();
     params['page'] = page;
     if (limit != null) params['limit'] = limit;
     if (name != null) params['name'] = name;
     if (type != null) params['type'] = type;
 
-    return _con.get('pokemon', queryParameters: params).then((response) =>
-        response['data']
+    return _api.get('pokemon', queryParameters: params).then((response) =>
+        response.data['data']
             .map<Pokemon>((item) => Pokemon.fromJson(item))
             .toList());
   }
 
   Future<List<PokemonType>> getPokemonTypes() {
-    return _con.get('pokemon/types').then((response) => response['data']
+    return _api.get('pokemon/types').then((response) => response.data['data']
         .map<PokemonType>((item) => PokemonType.fromJson(item))
         .toList());
   }
